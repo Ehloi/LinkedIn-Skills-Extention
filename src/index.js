8d036a16d7934627efa86215b712b67a176722ac
Eloi Honnet <eloi.honnet@epita.fr>
2022-10-22 21:59:11 +0100

    Clicking on the good answer, need to click on the next button and make it work for more than 1 question
    Signed-off-by: Eloi Honnet <eloi.honnet@epita.fr>

--- src/index.js
+++ src/index.js
@@ -1,66 +1,98 @@
 import { Q_A_Array } from "./answers.js"
 import * as levenshtein from 'fast-levenshtein';
-// get the question
-let main = document.querySelector('#main');
-const questionInnerText = main.querySelector("#assessment-a11y-title").innerText;
-let question = questionInnerText.split('\n')[0];
-console.log(question.split("\n")[0]);
-let answerLength = main.getElementsByTagName("li").length
-let possibilities = []
-for (let i = 0; i < answerLength; i++){
-    let possibility = main.querySelector(`#skill-assessment-quiz-${i} `).innerText;
-    // remove second line in possibility
-    possibility = possibility.split("\n")[0];
-    possibility.trim()
-    possibilities.push(possibility)
-    console.log(`Possibility: ${possibility}`)
-}
 
-// find the answer in ../answers.md
-let lines = Q_A_Array;
-// find the line with the shortest levenstein distance to the question in the answers.md 
-let answer = "";
-let minDistance = 1000;
-for (let i = 0; i < lines.length; i++){
-    let line = lines[i];
-    if (line.includes("####")){
-        let distance = levenshtein.get(question, line);
-        if (distance < minDistance){
-            minDistance = distance;
-            answer = lines[i+1];
+// set border to red of the document
+document.body.style.border = "5px solid red";
+
+console.log("Starting script");
+
+let main = document.querySelector("main");
+let questionInnerText = main.querySelector("#assessment-a11y-title").innerText;
+process(main,questionInnerText);
+async function process(main, questionInnerText){
+
+    let question = questionInnerText.split('\n')[0];
+    console.log(question);
+    let answerLength = main.getElementsByTagName("li").length
+    let possibilities = []
+
+    for (let i = 0; i < answerLength; i++){
+        let possibility = main.querySelector(`#skill-assessment-quiz-${i} `).innerText;
+        // remove second line in possibility
+        possibility = possibility.split("\n")[0];
+        possibility.trim()
+        possibilities.push(possibility)
+        console.log(`Possibility: ${possibility}`)
+    }
+
+    // find the answer in ../answers.md
+    let lines = Q_A_Array;
+    // find the line with the shortest levenstein distance to the question in the answers.md 
+    let answer = "";
+    let minDistance = 1000;
+    for (let i = 0; i < lines.length; i++){
+        let line = lines[i];
+        if (line.includes("####")){
+            let distance = levenshtein.get(question, line);
+            if (distance < minDistance){
+                minDistance = distance;
+                answer = lines[i+1];
+            }
         }
     }
-}
-// verify that the answer is correct
-if(answer.includes("x")){
-    // find the most probable answer
-    let minDistancePossibilities = 1000;
-    let answerIndex = 0;
-    let i = 0;
-    for (const possibilitie of possibilities)
-    {
-        let distance = levenshtein.get(answer, possibilitie);
-        if (distance < minDistancePossibilities){
-            minDistancePossibilities = distance;
-            answer = possibilitie;
-            answerIndex = i;
+    // verify that the answer is correct
+    if(answer.includes("x")){
+        console.log(`Answer from file ${answer}`)
+        // find the most probable answer
+        let minDistancePossibilities = 1000;
+        let answerIndex = 0;
+        let answerOnPage = '';
+        let i = 0;
+        for (const possibilitie of possibilities)
+        {
+            let distance = levenshtein.get(answer, possibilitie);
+            if (distance < minDistancePossibilities){
+                minDistancePossibilities = distance;
+                answerOnPage = possibilitie;
+                answerIndex = i;
+            }
+            console.log(`Possibility: ${possibilitie} Distance: ${distance}`)
+            i++;
         }
-        i++;
+        console.log(`Found the answer : ${answerOnPage} at index ${answerIndex}`)
+
+        // Select the list with class name list-style-none
+        let list = document.querySelector(".list-style-none");
+
+        // regex that matches id=\"urn:li:assessementQuestion:*\"
+        let regex = /urn:li:assessmentQuestion:[0-9]*-[0-9]/g;
+        let matches = list.innerHTML.match(regex);
+        
+        // remove all the element in odd idex in matches
+        for (let i = 0; i < matches.length; i++){
+            if (i % 2 == 1){
+                matches.splice(i, 1);
+            }
+        }
+
+        // get the id of the answer
+        let answerId = matches[answerIndex];
+
+        console.log(answerId)
+
+        // Click on the element with answerId
+        document.getElementById(`${answerId}`).click();
+    } else {
+        console.log("incorrect")
     }
-    console.log(`Found an answer : ${answer} to the question : ${question} at index ${answerIndex}`)
-
-    // click the correct answer with li class sa-question-multichoice__item
-    let queryRes = main.querySelector(`#sa-question-multichoice__item`)
-    console.info(`clicking: ${queryRes}`);
-    queryRes.click();
-
-    let correctAnswer = main.querySelector(`#skill-assessment-quiz-${i} `);
-    correctAnswer.click();
-    // click the next button with id:"ember28"
-    let nextButton = document.querySelector("#ember28");
-    nextButton.click();
-} else {
-    console.log("incorrect")
 }
-    
-//let questionLine = lines.findIndex((line) => line.startsWith(question));
+
+// wait for the question to change
+document.addEventListener('DOMSubtreeModified', function () {
+    if (questionInnerText != main.querySelector("#assessment-a11y-title").innerText){
+        console.log("Page changed, starting process");
+        main = document.querySelector("main");
+        questionInnerText = main.querySelector("#assessment-a11y-title").innerText;
+        process(main, questionInnerText);
+    }
+});
